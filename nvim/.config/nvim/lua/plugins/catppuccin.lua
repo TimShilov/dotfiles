fc04520d85bf49a10fa80c9e0f4aa2bff5c72da1
Tim Shilov <tim@shilov.dev>
2024-01-08 09:39:42 +0000

    feat(nvim): made background transparent

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -2,6 +2,9 @@ return {
   'catppuccin/nvim',
   name = "catppuccin",
   priority = 1000,
+  opts = {
+    transparent_background = true,
+  },
   config = function()
     vim.cmd.colorscheme 'catppuccin'
   end,
