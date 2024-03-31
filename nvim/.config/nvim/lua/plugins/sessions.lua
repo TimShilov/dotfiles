4a70e093bec268825cea8df184a567559e7371f4
Tim Shilov <tim@shilov.dev>
2024-03-31 22:18:12 +0100

    feat(nvim): added 'auto-session' plugin

---
+++ nvim/.config/nvim/lua/plugins/sessions.lua
@@ -0,0 +1,15 @@
+return {
+  'rmagatti/auto-session',
+
+  config = function()
+    require('auto-session').setup {
+      auto_session_suppress_dirs = { '~/', '~/Downloads', '~/Desktop' },
+      session_lens = {
+        buftypes_to_ignore = {},
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = true,
+      },
+    }
+  end,
+}
