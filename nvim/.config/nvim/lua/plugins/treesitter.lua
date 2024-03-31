c71137bddb52257c3e0d73984de1346c290d4db1
Tim Shilov <tim@shilov.dev>
2024-03-31 22:19:23 +0100

    refactor(nvim): removed 'ensure_installed' from treesitter in favor of auto install

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -9,10 +9,6 @@ return {
     -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
     vim.defer_fn(function()
       require('nvim-treesitter.configs').setup {
-        -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = { 'go', 'lua', 'tsx', 'javascript', 'typescript', 'vim', 'bash' },
-
-        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
         auto_install = true,
 
         highlight = { enable = true },
