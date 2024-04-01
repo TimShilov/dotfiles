a1979a9d6f90cfc1dd7284f14584c060a581b95e
Tim Shilov <tim@shilov.dev>
2024-04-01 15:10:29 +0100

    feat(nvim): added dressing for better UI

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,18 +1,27 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {},
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    'MunifTanjim/nui.nvim',
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    {
-      'rcarriga/nvim-notify',
-      opts = {
-        background_colour = '#000000',
+  opts = {
+    lsp = {
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
       },
     },
+    popupmenu = {
+      backend = 'cmp',
+    },
+    views = {
+      mini = {
+        win_options = {
+          winblend = 0,
+        },
+      },
+    },
+  },
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'hrsh7th/nvim-cmp',
   },
 }
