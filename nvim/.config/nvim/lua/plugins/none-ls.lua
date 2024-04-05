da4526070f0a4aad29382c55fee814a1423df569
Tim Shilov <tim@shilov.dev>
2024-04-05 19:12:34 +0100

    feat(nvim): added shfmt formatted and replaced prettierd with prettier

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -5,7 +5,8 @@ return {
     null_ls.setup {
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.shfmt,
         null_ls.builtins.formatting.sqlfluff.with {
           extra_args = { '--dialect', 'mysql' },
         },
