a1624d627a0f352f06ceba632ba93a344f4fe718
Tim Shilov <tim@shilov.dev>
2024-04-05 19:12:10 +0100

    feat(nvim): enabled tree-sitter folding

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -67,6 +67,10 @@ return {
           },
         },
       }
+      -- enable folding
+      vim.cmd [[set foldmethod=expr]]
+      vim.cmd [[set foldexpr=nvim_treesitter#foldexpr()]]
+      vim.cmd [[set nofoldenable]]
     end, 0)
   end,
 }
