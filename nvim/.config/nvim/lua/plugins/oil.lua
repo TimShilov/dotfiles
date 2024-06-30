42adadf5f814fa150e3c8783d598938908523d47
Tim Shilov <tim@shilov.dev>
2024-06-30 13:38:49 +0100

    feat(nvim): enabled hidden files in oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,11 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
+  },
   keys = {
     { '-', ':Oil<CR>', desc = 'Open parent directory' },
   },
