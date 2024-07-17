5970b2061064d7cdfb97848fba8a0454012b44b1
Tim Shilov <tim@shilov.dev>
2024-07-17 18:18:31 +0100

    feat: added nix flake with darwin and home manager

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "System Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }: {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#client-Tim-Shilov
+    darwinConfigurations."client-Tim-Shilov" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        ./modules/darwin
+        home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users."tim.shilov".imports = [ ./modules/home-manager ];
+          };
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."client-Tim-Shilov".pkgs;
+  };
+}
+
+
+
---
+++ modules/darwin/default.nix
@@ -0,0 +1,140 @@
+{ pkgs, ... }: {
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.orientation = "left";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    bat
+    cargo
+    eza
+    fd
+    fzf
+    gh
+    git
+    gofumpt
+    jq
+    k9s
+    htop
+    neovim
+    pam-reattach # to allow tmux to work nice with TouchID for sudo
+    skhd
+    yabai
+    yazi
+    zoxide
+  ];
+
+  # services.yabai.enable = true;
+  # services.yabai.package = pkgs.yabai;
+  # services.skhd.enable = true;
+  # services.skhd.package = pkgs.skhd;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;
+  environment = {
+    shellAliases = {
+      l = "ls -lh";
+    };
+    variables = {
+      LANG = "en_UK.UTF-8";
+    };
+  };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  nixpkgs.config.allowUnsupportedSystem = true;
+
+  security.pki.certificateFiles = [
+    "/etc/nix/ca_cert.pem"
+  ];
+
+  # Enable using touch id for sudo.
+  security.pam.enableSudoTouchIdAuth = true;
+
+  users.users."tim.shilov" = {
+    name = "tim.shilov";
+    home = "/Users/tim.shilov";
+  };
+
+  homebrew = {
+    enable = true;
+    caskArgs.no_quarantine = true;
+    global.brewfile = true;
+    onActivation = {
+      cleanup = "zap";
+      upgrade = true;
+    };
+    masApps = {
+      "Bitwarden" = 1352778147;
+      "Ghostery – Privacy Ad Blocker" = 1436953057;
+      "In Your Face" = 1476964367;
+      "Sequel Ace" = 1518036000;
+      "StudyCards" = 1534325530;
+      "Telegram" = 747648890;
+      "rcmd" = 1596283165;
+    };
+    casks = [
+      "aerospace"
+      "alacritty"
+      "arc"
+      "firefox@developer-edition"
+      "font-jetbrains-mono-nerd-font"
+      "gather"
+      "github"
+      "google-cloud-sdk"
+      "karabiner-elements"
+      "macs-fan-control"
+      "ngrok"
+      "nikitabobko/tap/aerospace"
+      "raycast"
+      "tableplus"
+    ];
+    taps = [
+      "FelixKratz/formulae"
+      "ankitpokhrel/jira-cli"
+      "hashicorp/tap"
+      "homebrew/bundle"
+      "homebrew/cask-fonts"
+      "homebrew/services"
+      "jesseduffield/lazygit"
+      "joshmedeski/sesh"
+      "nikitabobko/tap"
+      "noahgorstein/tap"
+    ];
+    brews = [
+      "asdf"
+      "borders"
+      "helm"
+      "jira-cli"
+      "joshmedeski/sesh/sesh"
+      "kubectx"
+      "kustomize"
+      "lazygit"
+      "lf"
+      "mas"
+      "ncdu"
+      "noahgorstein/tap/jqp"
+      "ripgrep"
+      "starship"
+      "stow"
+      "temporal"
+      "tmux"
+      "tokei"
+      "tree"
+      "watch"
+      "wget"
+      "zoxide"
+      "zsh-autosuggestions"
+    ];
+  };
+}
