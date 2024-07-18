137fde0290ef6a40d4eaa50ee1857153ce9f211f
Tim Shilov <tim@shilov.dev>
2024-07-18 13:07:36 +0100

    refactor(nix-darwin): cleaned up system packages

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -5,12 +5,12 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    btop
     cargo
     fd
     gofumpt
-    jq
-    btop
     htop
+    jq
     neovim
     pam-reattach # to allow tmux to work nice with TouchID for sudo
   ];
