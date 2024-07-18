70ee85780dc7c676b725dffd3443a34cf698388d
Tim Shilov <tim@shilov.dev>
2024-07-19 00:28:34 +0100

    feat(nvim): disabled oil confirmation for simple edits

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,6 +5,7 @@ return {
     view_options = {
       show_hidden = true,
     },
+    skip_confirm_for_simple_edits = true,
   },
   keys = {
     { '-', ':Oil<CR>', desc = 'Open parent directory' },
