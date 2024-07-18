765356012405d42e8e1bd61916b1236d4bdf4167
Tim Shilov <tim@shilov.dev>
2024-07-18 15:06:27 +0100

    refactor(home-manager): reorganized config a bit

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -64,126 +64,109 @@
     enable = true;
     flavor = "mocha";
   };
-  programs.eza = {
-    enable = true;
-    enableZshIntegration = true;
-  };
 
-  programs.k9s = {
-    enable = true;
-    aliases = {
+  programs = {
+    eza = { enable = true; enableZshIntegration = true; };
+
+    k9s = {
+      enable = true;
       aliases = {
-        dp = "apps/v1/deployments";
-        sec = "v1/secrets";
-        jo = "batch/v1/jobs";
-        cr = "rbac.authorization.k8s.io/v1/clusterroles";
-        crb = "rbac.authorization.k8s.io/v1/clusterrolebindings";
-        ro = "rbac.authorization.k8s.io/v1/roles";
-        rb = "rbac.authorization.k8s.io/v1/rolebindings";
-        np = "networking.k8s.io/v1/networkpolicies";
+        aliases = {
+          dp = "apps/v1/deployments";
+          sec = "v1/secrets";
+          jo = "batch/v1/jobs";
+          cr = "rbac.authorization.k8s.io/v1/clusterroles";
+          crb = "rbac.authorization.k8s.io/v1/clusterrolebindings";
+          ro = "rbac.authorization.k8s.io/v1/roles";
+          rb = "rbac.authorization.k8s.io/v1/rolebindings";
+          np = "networking.k8s.io/v1/networkpolicies";
+        };
       };
     };
-  };
 
-  programs.atuin = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-  programs.bat = {
-    enable = true;
-  };
-
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-    defaultCommand = "rg --files --hidden";
-  };
-
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-  programs.starship = {
-    enable = true;
-    # TODO: Enable after migrating config
-    # enableZshIntegration = true;
-  };
-
-  programs.gh = { enable = true; };
-  programs.gh-dash = { enable = true; };
-  programs.git = { enable = true; };
-  programs.yazi = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    autocd = true;
-    shellAliases = {
-      nixswitch = "darwin-rebuild switch --flake ~/.dotfiles/.#";
-      nixup = "pushd ~/.dotfiles; nix flake update; nixswitch; popd";
-
-      cat = "bat --paging=never";
+    atuin = { enable = true; enableZshIntegration = true; };
+    bat = { enable = true; };
+    fzf = { enable = true; enableZshIntegration = true; defaultCommand = "rg --files --hidden"; };
+    zoxide = { enable = true; enableZshIntegration = true; };
+    starship = {
+      enable = true;
+      # TODO: Enable after migrating config
+      # enableZshIntegration = true;
     };
-    history = {
-      expireDuplicatesFirst = true;
-      ignoreDups = true;
-      ignoreAllDups = true;
-      ignoreSpace = true;
-      save = 10000;
-      share = true;
-      size = 10000;
+
+    gh = { enable = true; };
+    gh-dash = { enable = true; };
+    git = { enable = true; };
+    yazi = { enable = true; enableZshIntegration = true; };
+
+    zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      autocd = true;
+      shellAliases = {
+        nixswitch = "darwin-rebuild switch --flake ~/.dotfiles/.#";
+        nixup = "pushd ~/.dotfiles; nix flake update; nixswitch; popd";
+
+        cat = "bat --paging=never";
+      };
+      history = {
+        expireDuplicatesFirst = true;
+        ignoreDups = true;
+        ignoreAllDups = true;
+        ignoreSpace = true;
+        save = 10000;
+        share = true;
+        size = 10000;
+      };
+      initExtraFirst = ''
+        source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+        source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
+
+        source <(kubectl completion zsh)
+        . /opt/homebrew/opt/asdf/libexec/asdf.sh
+      '';
+
+      initExtra = ''
+        function gc() {
+            local branches branch
+            branches=$(git branch --all | grep -v HEAD) &&
+                branch=$(echo "$branches" |
+                    fzf-tmux -d $((2 + $(wc -l <<<"$branches"))) +m) &&
+                git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+        }
+
+        function sc() {
+            if cat package.json >/dev/null 2>&1; then
+                selected_script=$(cat package.json | jq .scripts | sed '1d;$d' | fzf --cycle --height 80% --header="Press ENTER to run the script. ESC to quit.")
+
+                if [[ -n "$selected_script" ]]; then
+                    script_name=$(echo "$selected_script" | awk -F ': ' '{gsub(/"/, "", $1); print $1}' | awk '{$1=$1};1')
+
+                    print -s "npm run "$script_name
+                    npm run $script_name
+                else
+                    echo "Exit: You haven't selected any script"
+                fi
+            else
+                echo "Error: There's no package.json"
+            fi
+        }
+
+        # tabtab source for packages
+        # uninstall by removing these lines
+        [[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
+
+        # pnpm
+        export PNPM_HOME="$HOME/Library/pnpm"
+        case ":$PATH:" in
+        *":$PNPM_HOME:"*) ;;
+        *) export PATH="$PNPM_HOME:$PATH" ;;
+        esac
+        # pnpm end
+      '';
     };
-    initExtraFirst = ''
-      source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
-      source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
 
-      source <(kubectl completion zsh)
-      . /opt/homebrew/opt/asdf/libexec/asdf.sh
-    '';
-
-    initExtra = ''
-      function gc() {
-          local branches branch
-          branches=$(git branch --all | grep -v HEAD) &&
-              branch=$(echo "$branches" |
-                  fzf-tmux -d $((2 + $(wc -l <<<"$branches"))) +m) &&
-              git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-      }
-
-      function sc() {
-          if cat package.json >/dev/null 2>&1; then
-              selected_script=$(cat package.json | jq .scripts | sed '1d;$d' | fzf --cycle --height 80% --header="Press ENTER to run the script. ESC to quit.")
-
-              if [[ -n "$selected_script" ]]; then
-                  script_name=$(echo "$selected_script" | awk -F ': ' '{gsub(/"/, "", $1); print $1}' | awk '{$1=$1};1')
-
-                  print -s "npm run "$script_name
-                  npm run $script_name
-              else
-                  echo "Exit: You haven't selected any script"
-              fi
-          else
-              echo "Error: There's no package.json"
-          fi
-      }
-
-      # tabtab source for packages
-      # uninstall by removing these lines
-      [[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
-
-      # pnpm
-      export PNPM_HOME="$HOME/Library/pnpm"
-      case ":$PATH:" in
-      *":$PNPM_HOME:"*) ;;
-      *) export PATH="$PNPM_HOME:$PATH" ;;
-      esac
-      # pnpm end
-    '';
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
   };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
