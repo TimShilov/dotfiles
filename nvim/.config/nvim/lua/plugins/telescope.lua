dea33e166477af0692046df28e4b9988b9b3a9d9
Tim Shilov <tim@shilov.dev>
2024-07-20 11:44:25 +0100

    feat(nvim): some telescope tweaks

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -24,7 +24,8 @@ return {
           ['<C-d>'] = false,
         },
       },
-      file_ignore_patterns = { 'node_modules' },
+      file_ignore_patterns = { 'node_modules', 'package-lock.json' },
+      path_display = { 'truncate' },
     },
   },
   config = function()
@@ -83,7 +84,9 @@ return {
     end, { desc = '[/] Fuzzily search in current buffer' })
 
     vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sf', function()
+      builtin.find_files { hidden = true, file_ignore_patterns = { '.git' } }
+    end, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
