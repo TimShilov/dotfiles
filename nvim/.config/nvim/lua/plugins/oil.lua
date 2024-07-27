fa0ac4288e4b0bafbcc849ff3dde6cdd2839fa97
Tim Shilov <tim@shilov.dev>
2024-07-27 18:06:48 +0100

    fix(nvim): disabled oil keymaps conflicting with navigation

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,14 +1,34 @@
 return {
-  'stevearc/oil.nvim',
-  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-  opts = {
-    default_file_explorer = true,
-    view_options = {
-      show_hidden = true,
+  {
+    'stevearc/oil.nvim',
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    opts = {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
+      -- See :help oil-actions for a list of all available actions
+      keymaps = {
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = 'actions.close',
+        ['-'] = 'actions.parent',
+        ['_'] = 'actions.open_cwd',
+        ['`'] = 'actions.cd',
+        ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
+        ['gs'] = 'actions.change_sort',
+        ['gx'] = 'actions.open_external',
+        ['g.'] = 'actions.toggle_hidden',
+        ['g\\'] = 'actions.toggle_trash',
+      },
+      -- Set to false to disable all of the above keymaps
+      use_default_keymaps = false,
+      skip_confirm_for_simple_edits = true,
     },
-    skip_confirm_for_simple_edits = true,
-  },
-  keys = {
-    { '-', ':Oil<CR>', desc = 'Open parent directory' },
+    keys = {
+      { '-', ':Oil<CR>', desc = 'Open parent directory' },
+    },
   },
 }
