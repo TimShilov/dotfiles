d7ba1eb046af69fec9f99b37cbc703d57b33976e
Tim Shilov <tim@shilov.dev>
2024-08-13 17:24:42 +0100

    chore(nvim): changed GrugFar config

--- nvim/.config/nvim/lua/plugins/grug-far.lua
+++ nvim/.config/nvim/lua/plugins/grug-far.lua
@@ -4,12 +4,6 @@ return {
     -- disable automatic debounced search and trigger search when leaving insert mode instead
     searchOnInsertLeave = false,
 
-    -- specifies the command to run (with `vim.cmd(...)`) in order to create
-    -- the window in which the grug-far buffer will appear
-    -- ex (horizontal bottom right split): 'botright split'
-    -- ex (open new tab): 'tabnew %'
-    windowCreationCommand = 'tabnew %',
-
     -- whether or not to make a transient buffer which is both unlisted and fully deletes itself when not in use
     transient = false,
   },
