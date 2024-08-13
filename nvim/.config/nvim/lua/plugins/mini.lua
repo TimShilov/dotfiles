79f036e5ccc0ebcebcc5ddf367dc17782fd72200
Tim Shilov <tim@shilov.dev>
2024-08-13 17:23:54 +0100

    feat(nvim): added some more 'mini' plugins
    * mini.ai
    * mini.operators
    * mini.bracketed

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,8 +1,42 @@
 return {
-  { 'echasnovski/mini.surround', version = false },
-  { 'echasnovski/mini.icons', version = false },
+  {
+    'echasnovski/mini.surround',
+    version = false,
+    config = function()
+      require('mini.surround').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.ai',
+    version = false,
+  },
+  {
+    'echasnovski/mini.bracketed',
+    version = false,
+    config = function()
+      require('mini.bracketed').setup()
+    end,
+  },
   {
     'echasnovski/mini.comment',
     version = false,
+  },
+  {
+    'echasnovski/mini.operators',
+    version = false,
+    config = function()
+      require('mini.operators').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.icons',
+    version = false,
+  },
+  {
+    'echasnovski/mini.pairs',
+    version = false,
+    config = function()
+      require('mini.pairs').setup()
+    end,
   },
 }
