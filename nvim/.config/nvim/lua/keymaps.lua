d44206627ee089a3e1eee97ce62cfb2e7297c85f
Tim Shilov <tim@shilov.dev>
2024-08-13 17:26:29 +0100

    feat(nvim): adjusted keymaps
    * disabled 's' key as I don't use it and it conflicts with
    'mini-surround'
    * added some JSON schema helper keymaps (wrapping objects with 'oneOf'
    and 'anyOf')

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, 's', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -24,3 +25,6 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+vim.keymap.set('n', '<leader>jo', 'i{oneOf:[<esc>l%a]}<esc>', { desc = 'Wrap with oneOf' })
+vim.keymap.set('n', '<leader>ja', 'i{anyOf:[<esc>l%a]}<esc>', { desc = 'Wrap with anyOf' })
