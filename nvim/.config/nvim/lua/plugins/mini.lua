e1b7accaea22e1b7c06c855047af7eac5049d96d
Tim Shilov <tim@shilov.dev>
2024-08-13 20:14:36 +0100

    feat(nvim): added '[e' and ']e' keymaps to navigate errors

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -14,7 +14,16 @@ return {
     'echasnovski/mini.bracketed',
     version = false,
     config = function()
-      require('mini.bracketed').setup()
+      local MiniBracketed = require 'mini.bracketed'
+      MiniBracketed.setup()
+
+      local severity_error = vim.diagnostic.severity.ERROR
+      vim.keymap.set({ 'n', 'v' }, ']e', function()
+        MiniBracketed.diagnostic('forward', { severity = severity_error })
+      end, { desc = 'Next Error' })
+      vim.keymap.set({ 'n', 'v' }, '[e', function()
+        MiniBracketed.diagnostic('backward', { severity = severity_error })
+      end, { desc = 'Previous Error' })
     end,
   },
   {
