fb82a1d8c3a78597e520288d28f71f81d906f452
Tim Shilov <tim@shilov.dev>
2024-09-01 14:39:53 +0100

    feat(nvim): enabled auto-session

--- modules/home-manager/dotfiles/nvim/lua/plugins/sessions.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/sessions.lua
@@ -1,16 +1,8 @@
 return {
   'rmagatti/auto-session',
-  -- Temporary disabled to investigate if causing issues
-  enabled = false,
-  config = function()
-    require('auto-session').setup {
-      auto_session_suppress_dirs = { '~/', '~/Downloads', '~/Desktop' },
-      session_lens = {
-        buftypes_to_ignore = {},
-        load_on_setup = true,
-        theme_conf = { border = true },
-        previewer = true,
-      },
-    }
-  end,
+  enabled = true,
+  lazy = false,
+  opts = {
+    auto_session_suppress_dirs = { '~/', '~/IdeaProjects', '~/Downloads', '~/Desktop' },
+  },
 }
