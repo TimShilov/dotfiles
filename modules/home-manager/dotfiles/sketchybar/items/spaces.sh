89c6cee8cd950381e982d56be4c6cdfed2552040
Tim Shilov <tim@shilov.dev>
2024-09-23 00:29:25 +0100

    feat(sketchybar): disabled spaces

--- modules/home-manager/dotfiles/sketchybar/items/spaces.sh
+++ modules/home-manager/dotfiles/sketchybar/items/spaces.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env sh
 
-sketchybar --add event aerospace_workspace_change
-
-for sid in $(aerospace list-workspaces --all); do
-    color="$INACTIVE"
-    sketchybar --add item space.$sid left \
-        --subscribe space.$sid front_app_switched \
-        --subscribe space.$sid aerospace_workspace_change \
-        --set space.$sid \
-        icon="􀀁" \
-        background.drawing=off \
-        label.drawing=off \
-        icon.color="$color" \
-        icon.font.size=12 \
-        icon.align=center \
-        padding_left=0 \
-        padding_right=6 \
-        click_script="aerospace workspace $sid" \
-        script="$CONFIG_DIR/plugins/aerospace.sh $sid"
-done
+# sketchybar --add event aerospace_workspace_change
+#
+# for sid in $(aerospace list-workspaces --all); do
+#     color="$INACTIVE"
+#     sketchybar --add item space.$sid left \
+#         --subscribe space.$sid front_app_switched \
+#         --subscribe space.$sid aerospace_workspace_change \
+#         --set space.$sid \
+#         icon="􀀁" \
+#         background.drawing=off \
+#         label.drawing=off \
+#         icon.color="$color" \
+#         icon.font.size=12 \
+#         icon.align=center \
+#         padding_left=0 \
+#         padding_right=6 \
+#         click_script="aerospace workspace $sid" \
+#         script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+# done
