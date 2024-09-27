a02d19abeeaca9cb0e5c0ad8898f89f133c843bf
Tim Shilov <tim@shilov.dev>
2024-09-27 12:06:07 +0100

    fix(starship): character causing offset issues

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -128,6 +128,7 @@
       settings = {
         format = "$directory$git_branch$git_state$git_status$nix_shell$sudo$line_break$jobs$battery$character";
         right_format = "$cmd_duration";
+        character = { success_symbol = "[>](bold green)"; error_symbol = "[x](bold red)"; };
         git_branch = {
           format = "[\\($branch(:$remote_branch)\\)]($style) ";
           symbol = " ";
