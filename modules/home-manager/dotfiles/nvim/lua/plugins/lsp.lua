0abf97bcf5023fde3fbc5f9f2d3480fda36a3924
Tim Shilov <tim@shilov.dev>
2024-10-03 09:17:15 +0100

    chore(nvim): code formatted

--- modules/home-manager/dotfiles/nvim/lua/plugins/lsp.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,139 +1,140 @@
 return { -- LSP Configuration & Plugins
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-  },
-  config = function()
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
-      -- Create a function that lets us more easily define mappings specific LSP related items.
-      -- It sets the mode, buffer and description for us each time.
-      callback = function(event)
-        local map = function(mode, keys, func, desc)
-          if desc then
-            desc = 'LSP: ' .. desc
-          end
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+    },
+    config = function()
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+            -- Create a function that lets us more easily define mappings specific LSP related items.
+            -- It sets the mode, buffer and description for us each time.
+            callback = function(event)
+                local map = function(mode, keys, func, desc)
+                    if desc then
+                        desc = 'LSP: ' .. desc
+                    end
 
-          vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
-        end
+                    vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
+                end
 
-        map('n', '<leader>lr', '<cmd>LspRestart<cr>', '[L]SP [R]estart')
-        map('n', '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('n', '<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+                map('n', '<leader>lr', '<cmd>LspRestart<cr>', '[L]SP [R]estart')
+                map('n', '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+                map('n', '<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-        map('n', 'gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        -- map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-        map('n', 'gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('n', 'gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('n', '<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-        map('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        map('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                map('n', 'gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+                -- map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                map('n', 'gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                map('n', 'gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+                map('n', '<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+                map('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+                map('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+                    '[W]orkspace [S]ymbols')
 
-        -- See `:help K` for why this keymap
-        map('n', 'K', vim.lsp.buf.hover, 'Hover Documentation')
+                -- See `:help K` for why this keymap
+                map('n', 'K', vim.lsp.buf.hover, 'Hover Documentation')
 
-        -- Lesser used LSP functionality
-        map('n', 'gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                -- Lesser used LSP functionality
+                map('n', 'gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
+                -- The following two autocommands are used to highlight references of the
+                -- word under your cursor when your cursor rests there for a little while.
+                --    See `:help CursorHold` for information about when this is executed
+                --
+                -- When you move your cursor, the highlights will be cleared (the second autocommand).
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
 
-        if client and client.server_capabilities.documentHighlightProvider then
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.document_highlight,
-          })
+                if client and client.server_capabilities.documentHighlightProvider then
+                    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                        buffer = event.buf,
+                        callback = vim.lsp.buf.document_highlight,
+                    })
 
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.clear_references,
-          })
-        end
-      end,
-    })
+                    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                        buffer = event.buf,
+                        callback = vim.lsp.buf.clear_references,
+                    })
+                end
+            end,
+        })
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-    -- Enable the following language servers
-    local servers = {
-      ['js-debug-adapter'] = {},
-      ['nixpkgs-fmt'] = {},
-      ['sql-formatter'] = {},
-      actionlint = {},
-      bashls = {},
-      delve = {},
-      docker_compose_language_service = {},
-      dockerls = {},
-      eslint = {},
-      fixjson = {},
-      gopls = {},
-      groovyls = {},
-      hadolint = {},
-      helm_ls = {},
-      html = {},
-      jq = {},
-      jsonls = {},
-      nxls = {},
-      lua_ls = {
-        settings = {
-          Lua = {
-            runtime = { version = 'LuaJIT' },
-            workspace = {
-              checkThirdParty = false,
-              -- Tells lua_ls where to find all the Lua files that you have loaded
-              -- for your neovim configuration.
-              library = {
-                '${3rd}/luv/library',
-                unpack(vim.api.nvim_get_runtime_file('', true)),
-              },
-              -- If lua_ls is really slow on your computer, you can try this instead:
-              -- library = { vim.env.VIMRUNTIME },
+        -- Enable the following language servers
+        local servers = {
+            ['js-debug-adapter'] = {},
+            ['nixpkgs-fmt'] = {},
+            ['sql-formatter'] = {},
+            actionlint = {},
+            bashls = {},
+            delve = {},
+            docker_compose_language_service = {},
+            dockerls = {},
+            eslint = {},
+            fixjson = {},
+            gopls = {},
+            groovyls = {},
+            hadolint = {},
+            helm_ls = {},
+            html = {},
+            jq = {},
+            jsonls = {},
+            nxls = {},
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        runtime = { version = 'LuaJIT' },
+                        workspace = {
+                            checkThirdParty = false,
+                            -- Tells lua_ls where to find all the Lua files that you have loaded
+                            -- for your neovim configuration.
+                            library = {
+                                '${3rd}/luv/library',
+                                unpack(vim.api.nvim_get_runtime_file('', true)),
+                            },
+                            -- If lua_ls is really slow on your computer, you can try this instead:
+                            -- library = { vim.env.VIMRUNTIME },
+                        },
+                        completion = {
+                            callSnippet = 'Replace',
+                        },
+                        telemetry = { enable = false },
+                        diagnostics = { disable = { 'missing-fields' } },
+                    },
+                },
             },
-            completion = {
-              callSnippet = 'Replace',
+            omnisharp = {},
+            prettier = {},
+            prismals = {},
+            shfmt = {},
+            snyk = {},
+            sqlls = {},
+            stylua = {},
+            vacuum = {},
+            yamlls = {},
+        }
+
+        -- Ensure the servers and tools above are installed
+        require('mason').setup()
+
+        -- You can add other tools here that you want Mason to install
+        -- for you, so that they are available from within Neovim.
+        local ensure_installed = vim.tbl_keys(servers or {})
+        require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+        require('mason-lspconfig').setup {
+            handlers = {
+                function(server_name)
+                    local server = servers[server_name] or {}
+                    -- This handles overriding only values explicitly passed
+                    -- by the server configuration above. Useful when disabling
+                    -- certain features of an LSP (for example, turning off formatting for tsserver)
+                    server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                    require('lspconfig')[server_name].setup(server)
+                end,
             },
-            telemetry = { enable = false },
-            diagnostics = { disable = { 'missing-fields' } },
-          },
-        },
-      },
-      omnisharp = {},
-      prettier = {},
-      prismals = {},
-      shfmt = {},
-      snyk = {},
-      sqlls = {},
-      stylua = {},
-      vacuum = {},
-      yamlls = {},
-    }
-
-    -- Ensure the servers and tools above are installed
-    require('mason').setup()
-
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
-    local ensure_installed = vim.tbl_keys(servers or {})
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-    require('mason-lspconfig').setup {
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
-  end,
+        }
+    end,
 }
--- modules/home-manager/dotfiles/nvim/lua/plugins/none-ls.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/none-ls.lua
@@ -1,75 +1,75 @@
 return {
-  'nvimtools/none-ls.nvim',
-  dependencies = {
-    'nvimtools/none-ls-extras.nvim',
-    'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
-    'ThePrimeagen/refactoring.nvim',
-  },
-  config = function()
-    local null_ls = require 'null-ls'
-    local formatting = null_ls.builtins.formatting
-    local diagnostics = null_ls.builtins.diagnostics
-    local code_actions = null_ls.builtins.code_actions
+    'nvimtools/none-ls.nvim',
+    dependencies = {
+        'nvimtools/none-ls-extras.nvim',
+        'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
+        'ThePrimeagen/refactoring.nvim',
+    },
+    config = function()
+        local null_ls = require 'null-ls'
+        local formatting = null_ls.builtins.formatting
+        local diagnostics = null_ls.builtins.diagnostics
+        local code_actions = null_ls.builtins.code_actions
 
-    require('mason-null-ls').setup {
-      ensure_installed = {
-        'prettier',
-        'stylua',
-        'shfmt',
-      },
-      automatic_installation = true,
-    }
+        require('mason-null-ls').setup {
+            ensure_installed = {
+                'prettier',
+                'stylua',
+                'shfmt',
+            },
+            automatic_installation = true,
+        }
 
-    local sources = {
-      code_actions.refactoring,
-      formatting.prettier.with {
-        filetypes = {
-          'javascript',
-          'javascriptreact',
-          'typescript',
-          'typescriptreact',
-          'vue',
-          'css',
-          'scss',
-          'less',
-          'html',
-          'json',
-          'json5',
-          'jsonc',
-          'yaml',
-          'markdown',
-          'markdown.mdx',
-          'graphql',
-          'handlebars',
-          'svelte',
-          'astro',
-          'htmlangular',
-        },
-      },
-      formatting.stylua,
-      formatting.shfmt.with { args = { '-i', '4' } },
-      formatting.sql_formatter.with { command = { 'sleek' } },
-    }
+        local sources = {
+            code_actions.refactoring,
+            formatting.prettier.with {
+                filetypes = {
+                    'javascript',
+                    'javascriptreact',
+                    'typescript',
+                    'typescriptreact',
+                    'vue',
+                    'css',
+                    'scss',
+                    'less',
+                    'html',
+                    'json',
+                    'json5',
+                    'jsonc',
+                    'yaml',
+                    'markdown',
+                    'markdown.mdx',
+                    'graphql',
+                    'handlebars',
+                    'svelte',
+                    'astro',
+                    'htmlangular',
+                },
+            },
+            formatting.stylua,
+            formatting.shfmt.with { args = { '-i', '4' } },
+            formatting.sql_formatter.with { command = { 'sleek' } },
+        }
 
-    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-    null_ls.setup {
-      -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
-      sources = sources,
-      -- you can reuse a shared lspconfig on_attach callback here
-      on_attach = function(client, bufnr)
-        if client.supports_method 'textDocument/formatting' then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format { async = false }
+        local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+        null_ls.setup {
+            -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
+            sources = sources,
+            -- you can reuse a shared lspconfig on_attach callback here
+            on_attach = function(client, bufnr)
+                if client.supports_method 'textDocument/formatting' then
+                    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+                    vim.api.nvim_create_autocmd('BufWritePre', {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format { async = false }
+                        end,
+                    })
+                end
             end,
-          })
-        end
-      end,
-    }
+        }
 
-    vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format, { noremap = true, desc = 'Format buffer' })
-  end,
+        vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format, { noremap = true, desc = 'Format buffer' })
+    end,
 }
