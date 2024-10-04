4fba240c156d1ef269ca67ae76cdaf945c3b39d7
Tim Shilov <tim@shilov.dev>
2024-10-04 12:56:42 +0100

    feat(nvim): added 'nvim-notify' + plugin updates

--- modules/home-manager/dotfiles/nvim/lua/plugins/noice.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/noice.lua
@@ -1,26 +1,40 @@
 return {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-        'MunifTanjim/nui.nvim',
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    {
+      'rcarriga/nvim-notify',
+      opts = {
+        background_colour = '#000000',
+      },
     },
-    opts = {
-        lsp = {
-            override = {
-                ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                ['vim.lsp.util.stylize_markdown'] = true,
-                ['cmp.entry.get_documentation'] = true,
-            },
-        },
-        popupmenu = {
-            backend = 'cmp',
-        },
-        views = {
-            mini = {
-                win_options = {
-                    winblend = 0,
-                },
-            },
-        },
+  },
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
+      },
     },
+    views = {
+      mini = {
+        win_options = { winblend = 0 },
+      },
+      hover = {
+        border = { style = 'rounded' },
+        size = { max_width = 80 },
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = true,
+    },
+  },
 }
