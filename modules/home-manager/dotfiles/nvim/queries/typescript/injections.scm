12b23c9a5a22cdac5a95151bae2f855aebca051b
Tim Shilov <tim@shilov.dev>
2024-10-05 19:47:42 +0100

    feat(nvim) added query to inject SQL into typescript

---
+++ modules/home-manager/dotfiles/nvim/queries/typescript/injections.scm
@@ -0,0 +1,24 @@
+; Matching any strings directly after 'language=MySQL' comments
+(
+(comment) @comment (#contains? @comment "language=MySQL")
+.
+[
+  (string (string_fragment) @injection.content (#set! injection.language "sql"))
+  (template_string (string_fragment) @injection.content (#set! injection.language "sql"))
+]
+)
+; Pattern for string variables following a MySQL language comment
+(
+  (comment) @comment (#contains? @comment "language=MySQL")
+  .
+   (
+    lexical_declaration (
+       variable_declarator
+          value: ([
+            (string (string_fragment) @injection.content (#set! injection.language "sql"))
+            (template_string (string_fragment) @injection.content (#set! injection.language "sql"))
+          ])
+      )
+    )
+)
+
