34ed1d14ea383c8ee565c951f760cf8ade8a06df
Tim Shilov <tim@shilov.dev>
2024-10-06 15:31:08 +0100

    feat(nvim): added neotest and neotest-vitest

---
+++ modules/home-manager/dotfiles/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,26 @@
+return {
+    'nvim-neotest/neotest',
+    dependencies = {
+        'nvim-neotest/nvim-nio',
+        'nvim-lua/plenary.nvim',
+        'antoinemadec/FixCursorHold.nvim',
+        'nvim-treesitter/nvim-treesitter',
+        'marilari88/neotest-vitest',
+    },
+    config = function()
+        require('neotest').setup {
+            adapters = {
+                require 'neotest-vitest',
+            },
+        }
+        vim.api.nvim_set_keymap('n', '<leader>twr',
+            "<cmd>lua require('neotest').run.run({ vitestCommand = 'pnpm vitest run' })<cr>", { desc = 'Run Watch' })
+
+        vim.api.nvim_set_keymap(
+            'n',
+            '<leader>twf',
+            "<cmd>lua require('neotest').run.run({ vim.fn.expand('%'), vitestCommand = 'pnpm vitest run' })<cr>",
+            { desc = 'Run Watch File' }
+        )
+    end,
+}
