5d7edd7797971a45998d1cd6d6cc87d692e05996
Tim Shilov <tim@shilov.dev>
2024-10-07 19:27:23 +0100

    feat(karabiner): attempt to unify tmux-vim-navigator and aerospace keymaps

--- modules/home-manager/dotfiles/karabiner/test.sh
+++ modules/home-manager/dotfiles/karabiner/test.sh
@@ -2,4 +2,4 @@
 export PATH="/opt/homebrew/bin:/etc/profiles/per-user/$USER/bin:/run/current-system/sw/bin:$PATH"
 export PATH="$HOME/.nix-profile/bin:$PATH"
 
-tmux new-window "sesh connect $1"
+tmux new-window "$1"
