507584f696cd62317e8786313e062b70b69f419b
Tim Shilov <tim@shilov.dev>
2024-10-09 13:42:31 +0100

    style(nvim): auto-formatted injection queries

--- modules/home-manager/dotfiles/nvim/queries/c_sharp/injections.scm
+++ modules/home-manager/dotfiles/nvim/queries/c_sharp/injections.scm
@@ -1,6 +1,6 @@
-(
- assignment_expression
-   left: (identifier) @id (#eq? @id "PropertiesJsonSchema")
-   right: (raw_string_literal (raw_string_content) @injection.content (#set! injection.language "json"))
-)
-
+(assignment_expression
+  left: (identifier) @id
+  (#eq? @id "PropertiesJsonSchema")
+  right: (raw_string_literal
+    (raw_string_content) @injection.content
+    (#set! injection.language "json")))
--- modules/home-manager/dotfiles/nvim/queries/typescript/injections.scm
+++ modules/home-manager/dotfiles/nvim/queries/typescript/injections.scm
@@ -1,24 +1,27 @@
 ; Matching any strings directly after 'language=MySQL' comments
-(
-(comment) @comment (#contains? @comment "language=MySQL")
-.
-[
-  (string (string_fragment) @injection.content (#set! injection.language "sql"))
-  (template_string (string_fragment) @injection.content (#set! injection.language "sql"))
-]
-)
-; Pattern for string variables following a MySQL language comment
-(
-  (comment) @comment (#contains? @comment "language=MySQL")
+((comment) @comment
+  (#contains? @comment "language=MySQL")
   .
-   (
-    lexical_declaration (
-       variable_declarator
-          value: ([
-            (string (string_fragment) @injection.content (#set! injection.language "sql"))
-            (template_string (string_fragment) @injection.content (#set! injection.language "sql"))
-          ])
-      )
-    )
-)
+  [
+    (string
+      (string_fragment) @injection.content
+      (#set! injection.language "sql"))
+    (template_string
+      (string_fragment) @injection.content
+      (#set! injection.language "sql"))
+  ])
 
+; Pattern for string variables following a MySQL language comment
+((comment) @comment
+  (#contains? @comment "language=MySQL")
+  .
+  (lexical_declaration
+    (variable_declarator
+      value: [
+        (string
+          (string_fragment) @injection.content
+          (#set! injection.language "sql"))
+        (template_string
+          (string_fragment) @injection.content
+          (#set! injection.language "sql"))
+      ])))
