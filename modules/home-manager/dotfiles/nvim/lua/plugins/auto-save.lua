e37ca54906666307f4e65dc0f687bb566a6748d6
Tim Shilov <tim@shilov.dev>
2024-10-11 15:23:34 +0100

    chore(nvim): updated auto-save option name

--- modules/home-manager/dotfiles/nvim/lua/plugins/auto-save.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/auto-save.lua
@@ -5,7 +5,7 @@ return {
     trigger_events = {
       immediate_save = { 'BufLeave', 'FocusLost', 'VimLeavePre' },
       defer_save = { 'InsertLeave' },
-      cancel_defered_save = { 'InsertEnter' },
+      cancel_deferred_save = { 'InsertEnter' },
     },
     debounce_delay = 10000,
   },
