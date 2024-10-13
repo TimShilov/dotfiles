7b52e98ab3b4d13e2b475b8bd02dc783f61359d8
Tim Shilov <tim@shilov.dev>
2024-10-13 15:33:20 +0100

    feat(karabiner): cleanup + switched to karabiner.ts

--- modules/home-manager/dotfiles/karabiner/rules.ts
+++ modules/home-manager/dotfiles/karabiner/rules.ts
@@ -1,373 +1,50 @@
-import fs from "fs";
-import { KarabinerRules } from "./types";
-import { app, terminalCommand } from "./utils";
-import { TERMINAL_APP_ID } from "./constants";
+import {
+  layer,
+  map,
+  rule,
+  toApp,
+  withModifier,
+  writeToProfile,
+} from "karabiner.ts";
 
-const rules: KarabinerRules[] = [
-  // Define the Hyper key itself
-  {
-    description: "Hyper Key (⌃⌥⇧⌘)",
-    manipulators: [
-      {
-        description: "Caps Lock -> Hyper Key",
-        from: {
-          key_code: "caps_lock",
-          modifiers: {
-            optional: ["any"],
-          },
-        },
-        to: [
-          {
-            set_variable: {
-              name: "hyper",
-              value: 1,
-            },
-          },
-        ],
-        to_after_key_up: [
-          {
-            set_variable: {
-              name: "hyper",
-              value: 0,
-            },
-          },
-        ],
-        to_if_alone: [
-          {
-            key_code: "escape",
-          },
-        ],
-        type: "basic",
-      },
-    ],
-  },
-  app("Terminal", "t", { bundleId: TERMINAL_APP_ID }),
-  app("Browser", "w", { bundleId: "company.thebrowser.Browser" }),
-  app("Slack", "s", { bundleId: "com.tinyspeck.slackmacgap" }),
-  app("Spotify", "v", { bundleId: "com.spotify.client" }),
-  app("TablePlus", "d", { bundleId: "com.tinyapp.TablePlus" }),
-  app("TablePlus", "e", {
-    filePath: "/Applications/Karabiner-EventViewer.app",
-  }),
+writeToProfile("Default", [
+  rule("Caps Lock → Hyper").manipulators([
+    map("caps_lock").toHyper().toIfAlone("escape"),
+  ]),
+  terminalSessionsLayer(),
+  launchAppRules(),
+]);
 
-  //terminalCommand("Affluent", "a", "sesh connect -s affluent"),
+function launchAppRules() {
+  return rule("Launch Apps").manipulators([
+    withModifier("right_command")({
+      t: toApp("WezTerm"),
+      e: toApp("Karabiner-EventViewer.app"),
+      m: toApp("Mail"),
+      a: toApp("Activity Monitor"),
+      g: toApp("Gather"),
+      c: toApp("Calendar"),
+      w: toApp("Firefox Nightly"),
+      z: toApp("Zoom"),
+      s: toApp("Slack"),
+      v: toApp("Spotify"),
+      d: toApp("TablePlus"),
+    }),
+  ]);
+}
 
-  //...createHyperSubLayers({
-  //  spacebar: open(
-  //    "raycast://extensions/stellate/mxstbr-commands/create-notion-todo"
-  //  ),
-  //  // b = "B"rowse
-  //  b: {
-  //    t: open("https://twitter.com"),
-  //    // Quarterly "P"lan
-  //    p: open("https://mxstbr.com/cal"),
-  //    y: open("https://news.ycombinator.com"),
-  //    f: open("https://facebook.com"),
-  //    r: open("https://reddit.com"),
-  //    h: open("https://hashnode.com/draft"),
-  //  },
-  //  // o = "Open" applications
-  //  o: {
-  //    1: app("1Password"),
-  //    g: app("Google Chrome"),
-  //    c: app("Notion Calendar"),
-  //    v: app("Zed"),
-  //    d: app("Discord"),
-  //    s: app("Slack"),
-  //    e: app("Superhuman"),
-  //    n: app("Notion"),
-  //    // Open todo list managed via *H*ypersonic
-  //    h: open(
-  //      "notion://www.notion.so/stellatehq/7b33b924746647499d906c55f89d5026"
-  //    ),
-  //    z: app("zoom.us"),
-  //    // "M"arkdown (Reflect.app)
-  //    m: app("Reflect"),
-  //    r: app("Reflect"),
-  //    f: app("Finder"),
-  //    // "i"Message
-  //    i: app("Texts"),
-  //    p: app("Spotify"),
-  //    a: app("iA Presenter"),
-  //    // "W"hatsApp has been replaced by Texts
-  //    w: open("Texts"),
-  //    l: open(
-  //      "raycast://extensions/stellate/mxstbr-commands/open-mxs-is-shortlink"
-  //    ),
-  //  },
-  //
-  //  // TODO: This doesn't quite work yet.
-  //  // l = "Layouts" via Raycast's custom window management
-  //  // l: {
-  //  //   // Coding layout
-  //  //   c: shell`
-  //  //     open -a "Visual Studio Code.app"
-  //  //     sleep 0.2
-  //  //     open -g "raycast://customWindowManagementCommand?position=topLeft&relativeWidth=0.5"
-  //
-  //  //     open -a "Terminal.app"
-  //  //     sleep 0.2
-  //  //     open -g "raycast://customWindowManagementCommand?position=topRight&relativeWidth=0.5"
-  //  //   `,
-  //  // },
-  //
-  //  // w = "Window" via rectangle.app
-  //  //w: {
-  //  //  semicolon: {
-  //  //    description: "Window: Hide",
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "h",
-  //  //        modifiers: ["right_command"],
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  y: rectangle("previous-display"),
-  //  //  o: rectangle("next-display"),
-  //  //  k: rectangle("top-half"),
-  //  //  j: rectangle("bottom-half"),
-  //  //  h: rectangle("left-half"),
-  //  //  l: rectangle("right-half"),
-  //  //  f: rectangle("maximize"),
-  //  //  u: {
-  //  //    description: "Window: Previous Tab",
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "tab",
-  //  //        modifiers: ["right_control", "right_shift"],
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  i: {
-  //  //    description: "Window: Next Tab",
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "tab",
-  //  //        modifiers: ["right_control"],
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  n: {
-  //  //    description: "Window: Next Window",
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "grave_accent_and_tilde",
-  //  //        modifiers: ["right_command"],
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  b: {
-  //  //    description: "Window: Back",
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "open_bracket",
-  //  //        modifiers: ["right_command"],
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  // Note: No literal connection. Both f and n are already taken.
-  //  //  m: {
-  //  //    description: "Window: Forward",
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "close_bracket",
-  //  //        modifiers: ["right_command"],
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //},
-  //
-  //  // s = "System"
-  //  //s: {
-  //  //  u: {
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "volume_increment",
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  j: {
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "volume_decrement",
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  i: {
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "display_brightness_increment",
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  k: {
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "display_brightness_decrement",
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  l: {
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "q",
-  //  //        modifiers: ["right_control", "right_command"],
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  p: {
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "play_or_pause",
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  semicolon: {
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "fastforward",
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //  e: open(
-  //  //    `raycast://extensions/thomas/elgato-key-light/toggle?launchType=background`
-  //  //  ),
-  //  //  // "D"o not disturb toggle
-  //  //  d: open(
-  //  //    `raycast://extensions/yakitrak/do-not-disturb/toggle?launchType=background`
-  //  //  ),
-  //  //  // "T"heme
-  //  //  t: open(`raycast://extensions/raycast/system/toggle-system-appearance`),
-  //  //  c: open("raycast://extensions/raycast/system/open-camera"),
-  //  //  // 'v'oice
-  //  //  v: {
-  //  //    to: [
-  //  //      {
-  //  //        key_code: "spacebar",
-  //  //        modifiers: ["left_option"],
-  //  //      },
-  //  //    ],
-  //  //  },
-  //  //},
-  //
-  //  // v = "moVe" which isn't "m" because we want it to be on the left hand
-  //  // so that hjkl work like they do in vim
-  //  //v: {
-  //  //  h: {
-  //  //    to: [{ key_code: "left_arrow" }],
-  //  //  },
-  //  //  j: {
-  //  //    to: [{ key_code: "down_arrow" }],
-  //  //  },
-  //  //  k: {
-  //  //    to: [{ key_code: "up_arrow" }],
-  //  //  },
-  //  //  l: {
-  //  //    to: [{ key_code: "right_arrow" }],
-  //  //  },
-  //  //  // Magicmove via homerow.app
-  //  //  m: {
-  //  //    to: [{ key_code: "f", modifiers: ["right_control"] }],
-  //  //    // TODO: Trigger Vim Easymotion when VSCode is focused
-  //  //  },
-  //  //  // Scroll mode via homerow.app
-  //  //  s: {
-  //  //    to: [{ key_code: "j", modifiers: ["right_control"] }],
-  //  //  },
-  //  //  d: {
-  //  //    to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
-  //  //  },
-  //  //  u: {
-  //  //    to: [{ key_code: "page_down" }],
-  //  //  },
-  //  //  i: {
-  //  //    to: [{ key_code: "page_up" }],
-  //  //  },
-  //  //},
-  //
-  //  // c = Musi*c* which isn't "m" because we want it to be on the left hand
-  //  //c: {
-  //  //  p: {
-  //  //    to: [{ key_code: "play_or_pause" }],
-  //  //  },
-  //  //  n: {
-  //  //    to: [{ key_code: "fastforward" }],
-  //  //  },
-  //  //  b: {
-  //  //    to: [{ key_code: "rewind" }],
-  //  //  },
-  //  //},
-  //
-  //  // r = "Raycast"
-  //  //r: {
-  //  //  c: open("raycast://extensions/thomas/color-picker/pick-color"),
-  //  //  n: open("raycast://script-commands/dismiss-notifications"),
-  //  //  l: open(
-  //  //    "raycast://extensions/stellate/mxstbr-commands/create-mxs-is-shortlink"
-  //  //  ),
-  //  //  e: open(
-  //  //    "raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"
-  //  //  ),
-  //  //  p: open("raycast://extensions/raycast/raycast/confetti"),
-  //  //  a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
-  //  //  s: open("raycast://extensions/peduarte/silent-mention/index"),
-  //  //  h: open(
-  //  //    "raycast://extensions/raycast/clipboard-history/clipboard-history"
-  //  //  ),
-  //  //  1: open(
-  //  //    "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-1"
-  //  //  ),
-  //  //  2: open(
-  //  //    "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-2"
-  //  //  ),
-  //  //},
-  //}),
-  //{
-  //  description: "Change Backspace to Spacebar when Minecraft is focused",
-  //  manipulators: [
-  //    {
-  //      type: "basic",
-  //      from: {
-  //        key_code: "delete_or_backspace",
-  //      },
-  //      to: [
-  //        {
-  //          key_code: "spacebar",
-  //        },
-  //      ],
-  //      conditions: [
-  //        {
-  //          type: "frontmost_application_if",
-  //          file_paths: [
-  //            "^/Users/mxstbr/Library/Application Support/minecraft/runtime/java-runtime-gamma/mac-os-arm64/java-runtime-gamma/jre.bundle/Contents/Home/bin/java$",
-  //          ],
-  //        },
-  //      ],
-  //    },
-  //  ],
-  //},
-];
+function terminalSessionsLayer() {
+  const wrapCommand = (command: string) =>
+    `~/dotfiles/modules/home-manager/dotfiles/karabiner/tmux.sh '${command}'`;
+  const gotoSession = (session: string) =>
+    wrapCommand(`sesh connect -s ${session}`);
 
-fs.writeFileSync(
-  "karabiner.json",
-  JSON.stringify(
-    {
-      global: {},
-      profiles: [
-        {
-          name: "Default",
-          selected: true,
-          simple_modifications: [],
-          virtual_hid_keyboard: {
-            keyboard_type_v2: "ansi",
-          },
-          complex_modifications: {
-            rules,
-          },
-        },
-      ],
-    },
-    null,
-    2,
-  ),
-);
+  return layer("t", "Terminal Sessions")
+    .modifiers("right_command")
+    .manipulators([
+      map("a").to$(gotoSession("affluent")),
+      map("e").to$(gotoSession("affiliate-etl")),
+      map("d").to$(gotoSession("~/dotfiles")),
+      map("t").to$(gotoSession("token-registry")),
+    ]);
+}
