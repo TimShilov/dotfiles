c7da9028bcb26003bdb4d7cc0af96b99967ac519
Tim Shilov <tim@shilov.dev>
2024-10-22 18:41:21 +0100

    feat(nvim): increated conform formatting timeout

--- modules/home-manager/dotfiles/nvim/lua/plugins/conform.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/conform.lua
@@ -28,7 +28,7 @@ return {
     default_format_opts = {
       lsp_format = 'fallback',
     },
-    format_on_save = { timeout_ms = 1000 },
+    format_on_save = { timeout_ms = 3000 },
     formatters = {
       injected = {},
       shfmt = {
