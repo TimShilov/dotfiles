f7c1e5e7566c5716485711ec801903b015b86355
Tim Shilov <tim@shilov.dev>
2024-11-07 16:41:08 +0000

    fix(nvim): disabled TypeScrupt autofix for JS files

--- modules/home-manager/dotfiles/nvim/lua/plugins/typescript.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/typescript.lua
@@ -1,28 +1,28 @@
 return {
-    'pmizio/typescript-tools.nvim',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'neovim/nvim-lspconfig',
-        { 'dmmulroy/ts-error-translator.nvim', config = true },
-    },
-    config = function()
-        require('typescript-tools').setup {
-            on_attach = function(client)
-                client.server_capabilities.documentFormattingProvider = false
-                client.server_capabilities.documentRangeFormattingProvider = false
-            end,
-            expose_as_code_action = 'all',
-            code_lens = 'true',
-        }
+  'pmizio/typescript-tools.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'neovim/nvim-lspconfig',
+    { 'dmmulroy/ts-error-translator.nvim', config = true },
+  },
+  config = function()
+    require('typescript-tools').setup {
+      on_attach = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+      expose_as_code_action = 'all',
+      code_lens = 'true',
+    }
 
-        local typescript_on_save_augroup = vim.api.nvim_create_augroup('FormatOnSave', {})
-        vim.api.nvim_create_autocmd('BufWritePre', {
-            pattern = '*.ts,*.js,*.cjs,*.mjs',
-            group = typescript_on_save_augroup,
-            callback = function(event)
-                vim.cmd 'TSToolsRemoveUnusedImports sync'
-                vim.cmd 'TSToolsAddMissingImports sync'
-            end,
-        })
-    end,
+    local typescript_on_save_augroup = vim.api.nvim_create_augroup('FormatOnSave', {})
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = '*.ts',
+      group = typescript_on_save_augroup,
+      callback = function(event)
+        vim.cmd 'TSToolsRemoveUnusedImports sync'
+        vim.cmd 'TSToolsAddMissingImports sync'
+      end,
+    })
+  end,
 }
