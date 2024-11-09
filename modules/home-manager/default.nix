041c1679e4b6e8615406f950cce03c73ed29d4b1
Tim Shilov <tim@shilov.dev>
2024-11-09 11:13:31 +0000

    feat(home-manager): added launchd agent to run bugwarrior-pull

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -63,6 +63,26 @@ in
     # '')
   ];
 
+  launchd = {
+    enable = true;
+    agents = {
+      bugwarrior-pull = {
+        enable = true;
+        config = {
+          Label = "com.bugwarrior.pull";
+          # TODO: Find a way to not use macOS's built-in Python
+          Program = "/Users/tim.shilov/Library/Python/3.9/bin/bugwarrior-pull";
+          RunAtLoad = true;
+          ProcessType = "Background";
+          StartInterval = 900; # 15 minutes
+          EnvironmentVariables = {
+            PATH = "${config.home.profileDirectory}/bin:/usr/local/bin:/usr/bin:/bin";
+          };
+        };
+      };
+    };
+  };
+
   home.file = {
     ".config/borders/bordersrc" = {
       source = dotfiles/bordersrc;
