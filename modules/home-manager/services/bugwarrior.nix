aaced373368c2c82d975acf3f9f0769aec9d1689
Tim Shilov <tim@shilov.dev>
2024-11-12 20:28:23 +0000

    feat(bugwarrior): adjust configs

--- modules/home-manager/services/bugwarrior.nix
+++ modules/home-manager/services/bugwarrior.nix
@@ -13,7 +13,6 @@
           # TODO: Find a way to not use macOS's built-in Python
           Program = "/Users/tim.shilov/Library/Python/3.9/bin/bugwarrior-pull";
           RunAtLoad = true;
-          ProcessType = "Background";
           StartInterval = 900; # 15 minutes
           StandardErrorPath = "/Users/tim.shilov/bugwarrior-pull.err";
           StandardOutPath = "/Users/tim.shilov/bugwarrior-pull.out";
