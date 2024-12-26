0014ccaf18699f38a578935df3859726d706e499
Tim Shilov <tim@shilov.dev>
2024-12-26 16:06:00 +0000

    feat(bugwarrior): moved logs to '~/logs' folder + removed hardcoded home path

--- modules/home-manager/services/bugwarrior.nix
+++ modules/home-manager/services/bugwarrior.nix
@@ -11,11 +11,11 @@
         config = {
           Label = "dev.shilov.bugwarrior.pull";
           # TODO: Find a way to not use macOS's built-in Python
-          Program = "/Users/tim.shilov/Library/Python/3.9/bin/bugwarrior-pull";
+          Program = "${config.home.homeDirectory}/Library/Python/3.9/bin/bugwarrior-pull";
           RunAtLoad = true;
           StartInterval = 900; # 15 minutes
-          StandardErrorPath = "/Users/tim.shilov/bugwarrior-pull.err";
-          StandardOutPath = "/Users/tim.shilov/bugwarrior-pull.out";
+          StandardErrorPath = "${config.home.homeDirectory}/logs/bugwarrior-pull.err";
+          StandardOutPath = "${config.home.homeDirectory}/logs/bugwarrior-pull.out";
           EnvironmentVariables = {
             PATH = "${config.home.profileDirectory}/bin:/usr/local/bin:/usr/bin:/bin";
           };
