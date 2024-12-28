db7e0a0262c24696710da22be8e12352cce33dd4
Tim Shilov <tim@shilov.dev>
2024-12-28 21:07:35 +0000

    feat(nvim): enabled some catppuccin integrations

--- modules/home-manager/dotfiles/nvim/lua/plugins/catppuccin.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/catppuccin.lua
@@ -4,8 +4,39 @@ return {
   priority = 1000,
   opts = {
     transparent_background = true,
+    integrations = {
+      blink_cmp = true,
+      fidget = true,
+      grug_far = true,
+      copilot_vim = true,
+      neotest = true,
+      noice = true,
+      which_key = true,
+      dadbod_ui = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { 'italic' },
+          hints = { 'italic' },
+          warnings = { 'italic' },
+          information = { 'italic' },
+          ok = { 'italic' },
+        },
+        underlines = {
+          errors = { 'underline' },
+          hints = { 'underline' },
+          warnings = { 'underline' },
+          information = { 'underline' },
+          ok = { 'underline' },
+        },
+        inlay_hints = {
+          background = true,
+        },
+      },
+    },
   },
-  config = function()
+  config = function(_, opts)
+    require('catppuccin').setup(opts)
     vim.cmd.colorscheme 'catppuccin'
   end,
 }
