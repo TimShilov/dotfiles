8a0a034e856ec95c0f4fedca952a97c5d7806052
Tim Shilov <tim@shilov.dev>
2024-12-31 21:28:42 +0000

    feat(nvim): added timer status to lualine

--- modules/home-manager/dotfiles/nvim/lua/plugins/lualine.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,17 @@
 local theme = require 'lualine.themes.catppuccin'
 theme.normal.c.bg = nil
+local function get_timerly_status()
+  local state = require 'timerly.state'
+  if state.progress == 0 then
+    return ''
+  end
+
+  local total = math.max(0, state.total_secs + 1) -- Add 1 to sync with timer display
+  local mins = math.floor(total / 60)
+  local secs = total % 60
+
+  return string.format('%s %02d:%02d', state.mode:gsub('^%l', string.upper), mins, secs)
+end
 
 return {
   -- Set lualine as statusline
@@ -15,7 +27,7 @@ return {
     sections = {
       lualine_b = { 'branch', 'diagnostics' },
       lualine_c = { '%=', { 'filename', filestatus = true, path = 1 } },
-      lualine_x = { 'filetype' },
+      lualine_x = { 'filetype', get_timerly_status },
     },
   },
 }
