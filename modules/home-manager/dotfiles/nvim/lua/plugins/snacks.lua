cecd3c0c5b1032fac9ee74a5d4f0a3d7616264aa
Tim Shilov <tim@shilov.dev>
2025-01-25 18:05:05 +0000

    feat(nvim): adjusted snacks config + added few snacks

--- modules/home-manager/dotfiles/nvim/lua/plugins/snacks.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/snacks.lua
@@ -7,11 +7,14 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    bigfile = { enabled = true },
-    dashboard = { enabled = true },
-    indent = { enabled = true },
-    input = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
+    bigfile = {},
+    dashboard = {},
+    indent = {},
+    toggle = {},
+    input = {},
+    notifier = {},
+    quickfile = {},
+    statuscolumn = {},
+    words = {},
   },
 }
