d33280fa9bc51eb575417e07401197de70d04c22
Tim Shilov <tim@shilov.dev>
2025-01-31 19:52:26 +0000

    feat(nvim): added keymaps to jump between todo comments

--- modules/home-manager/dotfiles/nvim/lua/plugins/todo.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/todo.lua
@@ -6,4 +6,16 @@ return {
       keyword = 'fg',
     },
   },
+  config = function(_, opts)
+    local todo = require 'todo-comments'
+    todo.setup(opts)
+
+    vim.keymap.set('n', ']t', function()
+      todo.jump_next()
+    end, { desc = 'Next todo comment' })
+
+    vim.keymap.set('n', '[t', function()
+      todo.jump_prev()
+    end, { desc = 'Previous todo comment' })
+  end,
 }
