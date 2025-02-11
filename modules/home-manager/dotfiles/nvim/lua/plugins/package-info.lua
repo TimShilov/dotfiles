990e13b844c3d4be4e80fe666eb36802e9dfb24c
Tim Shilov <tim@shilov.dev>
2025-02-11 19:56:12 +0000

    feat(nvim): lazy-load package-info plugin

--- modules/home-manager/dotfiles/nvim/lua/plugins/package-info.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/package-info.lua
@@ -1,12 +1,13 @@
 return {
-    'vuki656/package-info.nvim',
-    dependencies = { 'MunifTanjim/nui.nvim' },
-    config = function()
-        require('package-info').setup {
-            hide_up_to_date = true,
-            hide_unstable_versions = true,
-        }
+  'vuki656/package-info.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  event = 'BufEnter package.json',
+  config = function()
+    require('package-info').setup {
+      hide_up_to_date = true,
+      hide_unstable_versions = true,
+    }
 
-        require('telescope').load_extension 'package_info'
-    end,
+    require('telescope').load_extension 'package_info'
+  end,
 }
