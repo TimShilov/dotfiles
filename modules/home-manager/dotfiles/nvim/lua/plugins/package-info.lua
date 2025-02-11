7f65f994fa314b6e8fc61d7899e688bc91657f32
Tim Shilov <tim@shilov.dev>
2025-02-11 21:10:51 +0000

    feat(nvim): disabled package-info for now because it conflicts with Octo

--- modules/home-manager/dotfiles/nvim/lua/plugins/package-info.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/package-info.lua
@@ -1,7 +1,11 @@
 return {
   'vuki656/package-info.nvim',
+  -- Disabled because it conflicts with Octo.nvim PR review
+  -- TODO: Figure out how to fix it
+  enabled = false,
   dependencies = { 'MunifTanjim/nui.nvim' },
   event = 'BufEnter package.json',
+  ft = 'json',
   config = function()
     require('package-info').setup {
       hide_up_to_date = true,
