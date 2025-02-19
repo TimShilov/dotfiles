86ab4345c6369e380dc68d40b762ca3d9a3652a9
Tim Shilov <tim@shilov.dev>
2025-02-19 23:21:10 +0000

    feat(nvim): added 'logsitter' plugin

---
+++ modules/home-manager/dotfiles/nvim/lua/plugins/logsitter.lua
@@ -0,0 +1,28 @@
+return {
+  'gaelph/logsitter.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  keys = {
+    {
+      '<leader>la',
+      function()
+        require('logsitter').log()
+      end,
+      desc = 'Log word under cursor',
+      mode = 'n',
+    },
+    {
+      '<leader>la',
+      function()
+        require('logsitter').log_visual()
+      end,
+      desc = 'Log selection',
+      mode = 'x',
+    },
+    {
+      '<leader>lc',
+      '<cmd>LogsitterClearBuff<CR>',
+      desc = 'Clear all logs',
+      mode = 'n',
+    },
+  },
+}
