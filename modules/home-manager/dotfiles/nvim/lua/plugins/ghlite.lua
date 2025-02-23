8016ea1ea537c157bd6c5b811dd9e3d6a7316b9a
Tim Shilov <tim@shilov.dev>
2025-02-23 18:17:52 +0000

    fixup! feat(nvim): added 'ghlite' plugin

--- modules/home-manager/dotfiles/nvim/lua/plugins/ghlite.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/ghlite.lua
@@ -1,39 +1,37 @@
 return {
   'daliusd/ghlite.nvim',
-  config = function()
-    require('ghlite').setup {
-      debug = false, -- if set to true debugging information is written to ~/.ghlite.log file
-      view_split = 'vsplit', -- set to empty string '' to open in active buffer, use 'tabnew' to open in tab
-      diff_split = 'vsplit', -- set to empty string '' to open in active buffer, use 'tabnew' to open in tab
-      comment_split = 'split', -- set to empty string '' to open in active buffer, use 'tabnew' to open in tab
-      open_command = 'open', -- open command to use, e.g. on Linux you might want to use xdg-open
-      merge = {
-        approved = '--squash',
-        nonapproved = '--auto --squash',
+  opts = {
+    debug = false, -- if set to true debugging information is written to ~/.ghlite.log file
+    view_split = 'vsplit', -- set to empty string '' to open in active buffer, use 'tabnew' to open in tab
+    diff_split = 'vsplit', -- set to empty string '' to open in active buffer, use 'tabnew' to open in tab
+    comment_split = 'split', -- set to empty string '' to open in active buffer, use 'tabnew' to open in tab
+    open_command = 'open', -- open command to use, e.g. on Linux you might want to use xdg-open
+    merge = {
+      approved = '--squash',
+      nonapproved = '--auto --squash',
+    },
+    html_comments_command = { 'lynx', '-stdin', '-dump' }, -- command to render HTML comments in PR view
+    -- override default keymaps with the ones you prefer
+    -- set keymap to false or '' to disable it
+    keymaps = {
+      diff = {
+        open_file = 'gf',
+        open_file_tab = '',
+        open_file_split = 'o',
+        open_file_vsplit = 'O',
+        approve = 'ca',
       },
-      html_comments_command = { 'lynx', '-stdin', '-dump' }, -- command to render HTML comments in PR view
-      -- override default keymaps with the ones you prefer
-      -- set keymap to false or '' to disable it
-      keymaps = {
-        diff = {
-          open_file = 'gf',
-          open_file_tab = '',
-          open_file_split = 'o',
-          open_file_vsplit = 'O',
-          approve = 'ca',
-        },
-        comment = {
-          send_comment = 'c<CR>', -- this one cannot be disabled
-        },
-        pr = {
-          approve = 'ca',
-          merge = 'cm',
-          comment = 'cc',
-          diff = 'cp',
-        },
+      comment = {
+        send_comment = 'c<CR>', -- this one cannot be disabled
       },
-    }
-  end,
+      pr = {
+        approve = 'ca',
+        merge = 'cm',
+        comment = 'cc',
+        diff = 'cp',
+      },
+    },
+  },
   keys = {
     { '<leader>us', ':GHLitePRSelect<cr>', silent = true, desc = 'PR Select' },
     { '<leader>uo', ':GHLitePRCheckout<cr>', silent = true, desc = 'PR Checkout' },
