25c0711530b4c71c9e6fc8bfb9db1caee4a50aa0
Tim Shilov <tim@shilov.dev>
2025-02-24 02:01:32 +0000

    chore: removed unused 'pay-respects' package

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -152,11 +152,6 @@
     mbsync = {
       enable = true;
     };
-    pay-respects = {
-      # TODO[2025-02-01]: Remove if not used
-      enable = true;
-      enableZshIntegration = true;
-    };
     notmuch = {
       enable = true;
       search = {
