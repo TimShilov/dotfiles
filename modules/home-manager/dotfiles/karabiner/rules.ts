c5d3e70fd918d92c28d7337e71dc23d3a6427b5d
Tim Shilov <tim@shilov.dev>
2025-03-01 12:29:58 +0000

    feat(karabiner): switched the browser hotkey to open Zen Browser

--- modules/home-manager/dotfiles/karabiner/rules.ts
+++ modules/home-manager/dotfiles/karabiner/rules.ts
@@ -1,58 +1,58 @@
 import {
-  layer,
-  map,
-  rule,
-  toApp,
-  withModifier,
-  writeToProfile,
+    layer,
+    map,
+    rule,
+    toApp,
+    withModifier,
+    writeToProfile,
 } from "karabiner.ts";
 import path from "path";
 
 const configFilePath = path.resolve("./karabiner.json");
 
 writeToProfile({ karabinerJsonPath: configFilePath, name: "Default" }, [
-  rule("Caps Lock → Hyper").manipulators([
-    map("caps_lock").toHyper().toIfAlone("escape"),
-  ]),
-  terminalSessionsLayer(),
-  launchAppRules(),
+    rule("Caps Lock → Hyper").manipulators([
+        map("caps_lock").toHyper().toIfAlone("escape"),
+    ]),
+    terminalSessionsLayer(),
+    launchAppRules(),
 ]);
 
 function launchAppRules() {
-  return rule("Launch Apps").manipulators([
-    withModifier("right_command")({
-      e: toApp("Karabiner-EventViewer.app"),
-      t: toApp("Ghostty"),
-      m: toApp("Mail"),
-      a: toApp("Activity Monitor"),
-      g: toApp("Gather"),
-      c: toApp("Calendar"),
-      w: toApp("Firefox Nightly"),
-      z: {
-        software_function: {
-          // @ts-expect-error This is a valid function. karabiner.ts just doesn't have a type for it.
-          open_application: { bundle_identifier: "us.zoom.xos" },
-        },
-      },
-      s: toApp("Slack"),
-      v: toApp("Spotify"),
-      d: toApp("TablePlus"),
-    }),
-  ]);
+    return rule("Launch Apps").manipulators([
+        withModifier("right_command")({
+            e: toApp("Karabiner-EventViewer.app"),
+            t: toApp("Ghostty"),
+            m: toApp("Mail"),
+            a: toApp("Activity Monitor"),
+            g: toApp("Gather"),
+            c: toApp("Calendar"),
+            w: toApp("Zen Browser"),
+            z: {
+                software_function: {
+                    // @ts-expect-error This is a valid function. karabiner.ts just doesn't have a type for it.
+                    open_application: { bundle_identifier: "us.zoom.xos" },
+                },
+            },
+            s: toApp("Slack"),
+            v: toApp("Spotify"),
+            d: toApp("TablePlus"),
+        }),
+    ]);
 }
 
 function terminalSessionsLayer() {
-  const wrapCommand = (command: string) =>
-    `~/dotfiles/modules/home-manager/dotfiles/karabiner/tmux.sh '${command}'`;
-  const gotoSession = (session: string) =>
-    wrapCommand(`sesh connect -s ${session}`);
+    const wrapCommand = (command: string) =>
+        `~/dotfiles/modules/home-manager/dotfiles/karabiner/tmux.sh '${command}'`;
+    const gotoSession = (session: string) =>
+        wrapCommand(`sesh connect -s ${session}`);
 
-  return layer("k", "Terminal Sessions")
-    .modifiers("right_command")
-    .manipulators([
-      map("a").to$(gotoSession("affluent")),
-      map("e").to$(gotoSession("affiliate-etl")),
-      map("d").to$(gotoSession("~/dotfiles")),
-      map("t").to$(gotoSession("token-registry")),
-    ]);
+    return layer("k", "Terminal Sessions")
+        .modifiers("right_command")
+        .manipulators([
+            map("a").to$(gotoSession("affluent")),
+            map("e").to$(gotoSession("affiliate-etl")),
+            map("d").to$(gotoSession("~/dotfiles")),
+            map("t").to$(gotoSession("token-registry")),
+        ]);
 }
