ab143d02280c03cde32e88f77261bec17318940f
Tim Shilov <tim@shilov.dev>
2025-03-27 18:14:08 +0000

    fix(ghostty): keymaps aren't working

--- modules/home-manager/programs/ghostty.nix
+++ modules/home-manager/programs/ghostty.nix
@@ -23,56 +23,56 @@
           "super+n=unbind"
           "super+shift+n=unbind"
 
-          # \x02 is Ctrl+b"
+          # \\x02 is Ctrl+b"
           # Previous tmux session (CMD + l)"
-          "super+l=text:\x02L"
+          "super+l=text:\\x02L"
 
           # Reload tmux config (CMD + r)"
-          "super+r=text:\x02r"
+          "super+r=text:\\x02r"
 
           # Open Lazygit (CMD + g)"
-          "super+g=text:\x02g"
+          "super+g=text:\\x02g"
 
           # Open Jira CLI (CMD + j)"
-          "super+j=text:\x02j"
+          "super+j=text:\\x02j"
 
           # Open Jira CLI - hotfixes only (CMD + J)"
-          "super+shift+j=text:\x02J"
+          "super+shift+j=text:\\x02J"
 
           # Open GitHub Dashboard (CMD + Shift + g)"
-          "super+shift+g=text:\x02G"
+          "super+shift+g=text:\\x02G"
 
           # Split vertically (CMD + d)"
-          "super+d=text:\x02n"
+          "super+d=text:\\x02n"
 
           # Split horizontally (CMD + Shift + d)"
-          "super+shift+d=text:\x02N"
+          "super+shift+d=text:\\x02N"
 
           # New tmux window (CMD + t)"
-          "super+t=text:\x02c"
+          "super+t=text:\\x02c"
 
           # Close tmux window (CMD + w)"
-          "super+w=text:\x02&"
+          "super+w=text:\\x02&"
 
           # tmux-fzf-url (CMD + u)"
-          "super+u=text:\x02u"
+          "super+u=text:\\x02u"
 
           # kubernetes (CMD + e)"
-          "super+e=text:\x02e"
+          "super+e=text:\\x02e"
 
           # Zoom in tmux pane (CMD + f)"
-          "super+f=text:\x02z"
+          "super+f=text:\\x02z"
 
           # Tmux window navigation (CMD + [/])"
-          "super+left_bracket=text:\x02p"
-          "super+right_bracket=text:\x02]"
+          "super+left_bracket=text:\\x02p"
+          "super+right_bracket=text:\\x02]"
 
           # Switch to tmux window (CMD + 1..5)"
-          "super+1=text:\x021"
-          "super+2=text:\x022"
-          "super+3=text:\x023"
-          "super+4=text:\x024"
-          "super+5=text:\x025"
+          "super+1=text:\\x021"
+          "super+2=text:\\x022"
+          "super+3=text:\\x023"
+          "super+4=text:\\x024"
+          "super+5=text:\\x025"
         ];
       };
     };
