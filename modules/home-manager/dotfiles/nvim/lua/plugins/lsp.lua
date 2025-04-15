1644e9e838b28a0be71c882500ce20a2ed3d7514
Tim Shilov <tim@shilov.dev>
2025-04-15 14:37:46 +0100

    feat(nvim): removed custom LSP keymaps to use built-in ones + cleanup

--- modules/home-manager/dotfiles/nvim/lua/plugins/lsp.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/lsp.lua
@@ -24,35 +24,6 @@ return { -- LSP Configuration & Plugins
         end
 
         map('n', '<leader>lr', '<cmd>LspRestart<cr>', '[L]SP [R]estart')
-        map('n', '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('n', '<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-        -- map('n', 'gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        -- map('n', 'gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-        -- map('n', 'gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        -- map('n', 'gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        -- map('n', '<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-        -- map('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        -- map('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
-        -- local client = vim.lsp.get_client_by_id(event.data.client_id)
-        --
-        -- if client and client.server_capabilities.documentHighlightProvider then
-        --   vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-        --     buffer = event.buf,
-        --     callback = vim.lsp.buf.document_highlight,
-        --   })
-        --
-        --   vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-        --     buffer = event.buf,
-        --     callback = vim.lsp.buf.clear_references,
-        --   })
-        -- end
 
         vim.api.nvim_create_autocmd('BufWritePre', {
           pattern = { '*.[jt]sx', '*.[mc][jt]s', '*.[jt]s' },
