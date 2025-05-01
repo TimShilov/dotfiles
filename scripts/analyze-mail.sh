26cf42812406789a5064b9421ba854b9938f2867
Tim Shilov <tim@shilov.dev>
2025-05-01 10:42:14 +0100

    feat(scripts): added messages by mailbox to 'analyze-email'

--- scripts/analyze-mail.sh
+++ scripts/analyze-mail.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo 'Messages by mailbox:'
+notmuch search --output=files '*' | xargs -n1 dirname | sort | uniq -c | sort -nr  | awk '{sum += $1; print} END {print sum, "total"}'
+
 echo 'Top 10 most frequent senders:'
 for sender in $(notmuch address --output=address --deduplicate=address '*'); do
     count=$(notmuch count from:"$sender")
