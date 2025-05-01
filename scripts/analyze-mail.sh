990ec92a4fa70bb3a911af4b52f7604f4f8b9a9d
Tim Shilov <tim@shilov.dev>
2025-05-01 10:59:02 +0100

    fix(scripts): 'messages by mailbox' breaks when there are spaces in the file path

--- scripts/analyze-mail.sh
+++ scripts/analyze-mail.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 echo 'Messages by mailbox:'
-notmuch search --output=files '*' | xargs -n1 dirname | sort | uniq -c | sort -nr  | awk '{sum += $1; print} END {print sum, "total"}'
+notmuch search --output=files a |
+  while IFS= read -r file; do dirname "$file"; done |
+  sort |
+  uniq -c |
+  sort -nr |
+  awk '{count=$1; $1=""; sub(/^ /, ""); sum+=count; print count, $0} END {print sum, "total"}'
 
 echo 'Top 10 most frequent senders:'
 for sender in $(notmuch address --output=address --deduplicate=address '*'); do
-    count=$(notmuch count from:"$sender")
-    echo "$count $sender"
+  count=$(notmuch count from:"$sender")
+  echo "$count $sender"
 done | sort -nr | head -n 10
