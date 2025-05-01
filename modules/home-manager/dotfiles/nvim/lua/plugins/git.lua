b7d9fc2261ef37b745121c001ceb67d868cec97b
Tim Shilov <tim@shilov.dev>
2025-05-01 17:36:06 +0100

    refactor(nvim): cleaned up gitsigns keymaps

--- modules/home-manager/dotfiles/nvim/lua/plugins/git.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/git.lua
@@ -31,16 +31,10 @@ return {
         end, { desc = 'Previous hunk' })
 
         -- Actions
-        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage hunk' })
         map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Reset hunk' })
-        map('v', '<leader>hs', function()
-          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'Stage hunk' })
         map('v', '<leader>hr', function()
           gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = 'Reset hunk' })
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage buffer' })
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'Undo stage hunk' })
         map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Reset buffer' })
         map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Preview hunk' })
         map('n', '<leader>hb', function()
@@ -51,7 +45,7 @@ return {
         map('n', '<leader>hD', function()
           gitsigns.diffthis '~'
         end, { desc = 'Diff this' })
-        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = 'Toggle deleted' })
+        map('n', '<leader>td', gitsigns.preview_hunk_inline, { desc = 'Toggle deleted' })
 
         -- Text object
         map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
