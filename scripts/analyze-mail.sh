a351c40835ad5b386f17cfdf291ac40decac6c8b
Tim Shilov <tim@shilov.dev>
2025-05-03 11:22:12 +0100

    feat(analyze-email): added ability to pass filter as argument

--- scripts/analyze-mail.sh
+++ scripts/analyze-mail.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+FILTER=${1:-'*'}
+
 echo 'Messages by mailbox:'
-notmuch search --output=files '*' |
+notmuch search --output=files "$FILTER" |
   while IFS= read -r file; do dirname "$file"; done |
   sed -E 's:/(cur|new)$::' |
   sort |
@@ -9,8 +11,9 @@ notmuch search --output=files '*' |
   sort -nr |
   awk '{count=$1; $1=""; sub(/^ /, ""); sum+=count; print count, $0} END {print sum, "total"}'
 
+echo "-------------------"
 echo 'Top 10 most frequent senders:'
-for sender in $(notmuch address --output=address --deduplicate=address '*'); do
+for sender in $(notmuch address --output=address --deduplicate=address "$FILTER"); do
   count=$(notmuch count from:"$sender")
   echo "$count $sender"
 done | sort -nr | head -n 10
