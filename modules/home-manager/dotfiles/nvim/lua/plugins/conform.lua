3b26024358db5ffce05521475b4efb3c46b6e425
Tim Shilov <tim@shilov.dev>
2025-05-17 00:52:28 +0100

    refactor(nvim): refactored conform to run prettier on more filetypes

--- modules/home-manager/dotfiles/nvim/lua/plugins/conform.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/conform.lua
@@ -1,3 +1,33 @@
+local prettier_langs = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'css', 'html', 'json', 'yaml' }
+
+---@module "conform"
+---@type conform.setupOpts
+local options = {
+  formatters_by_ft = {
+    lua = { 'stylua' },
+    query = { 'format-queries' },
+    nix = { 'nixfmt' },
+    terraform = { 'terraform_fmt' },
+    sh = { 'shfmt' },
+    go = { 'goimports', 'gofumpt' },
+    ['*'] = { 'injected' },
+  },
+  default_format_opts = {
+    lsp_format = 'fallback',
+  },
+  format_on_save = { timeout_ms = 3000, lsp_format = 'fallback' },
+  formatters = {
+    injected = {},
+    shfmt = {
+      prepend_args = { '-i', '2' },
+    },
+  },
+}
+
+for _, lang in ipairs(prettier_langs) do
+  options.formatters_by_ft[lang] = { 'prettier' }
+end
+
 return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
@@ -12,32 +42,7 @@ return {
       desc = 'Code format',
     },
   },
-  -- This will provide type hinting with LuaLS
-  ---@module "conform"
-  ---@type conform.setupOpts
-  opts = {
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      javascript = { 'prettier', stop_after_first = true },
-      typescript = { 'prettier', stop_after_first = true },
-      json = { 'prettier', stop_after_first = true },
-      query = { 'format-queries' },
-      nix = { 'nixfmt' },
-      sh = { 'shfmt' },
-      go = { 'goimports', 'gofumpt' },
-      ['*'] = { 'injected' },
-    },
-    default_format_opts = {
-      lsp_format = 'fallback',
-    },
-    format_on_save = { timeout_ms = 3000, lsp_format = 'fallback' },
-    formatters = {
-      injected = {},
-      shfmt = {
-        prepend_args = { '-i', '2' },
-      },
-    },
-  },
+  opts = options,
   init = function()
     -- If you want the formatexpr, here is the place to set it
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
