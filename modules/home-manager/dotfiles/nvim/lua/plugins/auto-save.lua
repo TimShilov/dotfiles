9401a1cfbc8a1453bf96e66cc9624064ef7e43da
Tim Shilov <tim@shilov.dev>
2025-05-25 11:17:47 +0100

    feat(nvim): updated auto-save config to allow excluding filetypes and files

--- modules/home-manager/dotfiles/nvim/lua/plugins/auto-save.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/auto-save.lua
@@ -1,8 +1,22 @@
+local excluded_filetypes = {
+  'gitcommit',
+}
+local excluded_filenames = {}
+
+local function save_condition(buf)
+  if vim.tbl_contains(excluded_filetypes, vim.fn.getbufvar(buf, '&filetype')) or vim.tbl_contains(excluded_filenames, vim.fn.expand '%:t') then
+    return false
+  end
+
+  return true
+end
+
 return {
   'okuuva/auto-save.nvim',
   event = { 'InsertLeave', 'TextChanged' },
   opts = {
     enabled = true,
+    condition = save_condition,
     trigger_events = {
       immediate_save = { 'BufLeave', 'FocusLost', 'VimLeavePre' },
       defer_save = { 'InsertLeave' },
