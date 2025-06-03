263d68c97c1672e4415d9f21168ca68ea1cb6a41
Tim Shilov <tim@shilov.dev>
2025-06-03 21:29:49 +0100

    Revert "fix: removed '--insecure-skip-tls-verify' from 'k' alias"
    This reverts commit 8a83526bb9f4d2bbb839174286b106c35a56b06d.

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-k9s --context=$(kubectl config get-contexts --no-headers | sed 's/*//' | awk '{ print $1}' | fzf)
+k9s --insecure-skip-tls-verify \
+    --context=$(kubectl config get-contexts --no-headers | sed 's/*//' | awk '{ print $1}' | fzf)
