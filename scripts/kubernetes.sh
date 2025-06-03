8a83526bb9f4d2bbb839174286b106c35a56b06d
Tim Shilov <tim@shilov.dev>
2025-06-03 21:07:47 +0100

    fix: removed '--insecure-skip-tls-verify' from 'k' alias

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-k9s --insecure-skip-tls-verify \
-    --context=$(kubectl config get-contexts --no-headers | sed 's/*//' | awk '{ print $1}' | fzf)
+k9s --context=$(kubectl config get-contexts --no-headers | sed 's/*//' | awk '{ print $1}' | fzf)
