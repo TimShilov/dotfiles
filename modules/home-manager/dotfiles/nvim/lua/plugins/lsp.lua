bfff0f7fe0d079ff75868f5f3cec7d2c76141516
Tim Shilov <tim@shilov.dev>
2025-08-18 12:01:06 +0100

    feat(nvim): run eslint fix on save

--- modules/home-manager/dotfiles/nvim/lua/plugins/lsp.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/lsp.lua
@@ -81,5 +81,20 @@ return { -- LSP Configuration & Plugins
     -- TODO: Find a way to use Mason to install cspell_ls
     vim.lsp.enable 'cspell_ls'
     vim.lsp.enable 'tsgo'
+
+    local base_on_attach = vim.lsp.config.eslint.on_attach
+    vim.lsp.config('eslint', {
+      on_attach = function(client, bufnr)
+        if not base_on_attach then
+          return
+        end
+
+        base_on_attach(client, bufnr)
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          buffer = bufnr,
+          command = 'LspEslintFixAll',
+        })
+      end,
+    })
   end,
 }
